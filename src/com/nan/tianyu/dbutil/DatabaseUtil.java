dda9f009500f5ae066f4e0477696d3b61f3c55aa
tnan3 <tnan3@gatech.edu>
2017-12-27 18:15:12 -0500

    initial change

--- src/com/nan/tianyu/dbutil/DatabaseUtil.java
+++ src/com/nan/tianyu/dbutil/DatabaseUtil.java
@@ -40,14 +40,14 @@ public class DatabaseUtil {
 		Connection conn = null;
 		Statement stmt = null;
 		try {
-			// STEP 2: Register JDBC driver
+
 			Class.forName("com.mysql.jdbc.Driver");
 
-			// STEP 3: Open a connection
+
 			System.out.println("Connecting to database...");
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 
-			// STEP 4: Execute a query
+
 			System.out.println("Creating statement...");
 			stmt = conn.createStatement();
 			String sql;
