75e271bd575c4a80f62cc4f4ff59147c72144bf9
tnan3 <tnan3@gatech.edu>
2017-12-28 11:43:30 -0500

    user group change

---
+++ src/com/nan/tianyu/model/UserBean.java
@@ -0,0 +1,90 @@
+package com.nan.tianyu.model;
+
+import java.util.Date;
+
+public class UserBean implements java.io.Serializable {
+	private int userID;
+	private String userName;
+	private String firstName="";
+	private String lastName="";
+	private String email;
+	private String passowrd;
+	private String message="";
+	private int status;
+	private Date createDate;
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public UserBean() {
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassowrd() {
+		return passowrd;
+	}
+
+	public void setPassowrd(String passowrd) {
+		this.passowrd = passowrd;
+	}
+}
